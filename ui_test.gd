fe90c4810d74e4540a99847b29ac459533f20159
carlos padial <carlos@asturnazari.com>
2024-06-28 20:55:19 +0200

    object inspector example

---
+++ ui_test.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var testNode = $Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$ObjectInspector.set_object(testNode)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
